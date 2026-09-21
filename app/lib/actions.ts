fb1b769facb192374c45b1750fcd6aaa3892dc54
TBustosGH <tnbustos2007@gmail.com>
2026-09-21 13:14:52 -0300

    Feat: end of chapter 13 ---Improving accessibility--- -Add lint script in package.json -Downgrade the devDependency eslint version to 9.39.5, to fix compatibility issues -Create eslint.config.mjs file in root -Add type State in app/lib/actions.ts -Change parse function to safeParse in functions createInvoice and updateInvoice in app/lib/actionsts -Change { customerId, amount, status } vars to validatedFields var for compatibility with the return value of safeParse function in functions createInvoice & updateInvoice -Add and if statement to check the success of the safeParse functions in createInvoice & updateInvoice in app/lib/actions.ts -import State & useActionState into Form (app/ui/invoices/create-form.tsx) & EditInvoiceForm (app/ui/invoices/edit-form.tsx) components -Add aria-describedby to every input tag in Form component -Add four div tag (with IDs: customer-error, amount-error, status-error, form-error) in Form component, to describe any validation error of the inputs in the form tag -Create an useActionState hook and pass its formAction to the action attribute of the form tag of the Form component -Create an useActionState hook and pass its formAction to the action attribute of the form tag of the EditInvoiceForm component

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -9,25 +9,51 @@ const sql = postgres(process.env.STORAGE_POSTGRES_URL!, { ssl: 'require' });
 
 const FormSchema = z.object({
     id: z.string(),
-    customerId: z.string(),
-    amount: z.coerce.number(),
-    status: z.enum(['pending', 'paid']),
+    customerId: z.string({
+        invalid_type_error: 'Please select a customer.',
+    }),
+    amount: z.coerce
+        .number()
+        .gt(0, { message: 'Please enter an amount greater than $0.' }),
+    status: z.enum(['pending', 'paid'], {
+        invalid_type_error: 'Please select an invoice status.',
+    }),
     date: z.string(),
 });
 
 const CreateInvoice = FormSchema.omit({ id: true, date: true });
 const UpdateInvoice = FormSchema.omit({ id: true, date: true });
 
-export async function createInvoice(formdata: FormData) {
-    const { customerId, amount, status } = CreateInvoice.parse({
+export type State = {
+    errors?: {
+        customerId?: string[];
+        amount?: string[];
+        status?: string[];
+    };
+    message?: string | null;
+};
+
+export async function createInvoice(prevState: State, formdata: FormData) {
+    const validatedFields = CreateInvoice.safeParse({
         customerId: formdata.get('customerId'),
         amount: formdata.get('amount'),
         status: formdata.get('status'),
     });
 
+    // If form validation fails, return errors early. Otherwise, continue.
+    if (!validatedFields.success) {
+        return {
+            errors: validatedFields.error.flatten().fieldErrors,
+            message: 'Missing fields. Failed to Create Invoice.',
+        };
+    }
+
+    // Prepare data for insertion into the database
+    const { customerId, amount, status } = validatedFields.data;
     const amountInCents = amount * 100;
     const date = new Date().toISOString().split('T')[0];
 
+    // Insert data into the database
     try {
         await sql`
         INSERT INTO 
@@ -55,13 +81,24 @@ export async function createInvoice(formdata: FormData) {
     redirect('/dashboard/invoices');
 }
 
-export async function updateInvoice(id: string, formdata: FormData) {
-    const { customerId, amount, status } = UpdateInvoice.parse({
+export async function updateInvoice(
+    id: string,
+    prevState: State,
+    formdata: FormData,
+) {
+    const validatedFields = UpdateInvoice.safeParse({
         customerId: formdata.get('customerId'),
         amount: formdata.get('amount'),
         status: formdata.get('status'),
     });
 
+    if(!validatedFields.success) {
+        return {
+            errors: validatedFields.error.flatten().fieldErrors,
+            message: 'Missing Fields. Failed to Update Invoice.',
+        };
+    }
+    const { customerId, amount, status } = validatedFields.data;
     const amountInCents = amount * 100;
 
     try {
